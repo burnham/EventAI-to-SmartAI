burnham/EventAI-to-SmartAI

commit 8ceba09f902c456b579ef6a2e509a4297dd33492

Tools/PHP:
* Fixed the comment for SMART_ACTION_TALK.
* Fixed the output SQL string's structure (missing coma).
* Do not extract a whole spell entry from the DBCs.
* Use elseif to improve processing time.

Time went down from ~81 seconds to ~51 seconds.

PHP/sqlbuilder.class.php
@@ -271,7 +271,7 @@ class SAI
                 $outputString .= $summonData->orientation . ',';
             }
             else
-                $outputString .= $this->data['actions'][$i]['target'] . '0,0,0,0,0,0,0,';
+                $outputString .= $this->data['actions'][$i]['target'] . ',0,0,0,0,0,0,0,';
 
             # Build the comment, and we're done.
             
@@ -297,29 +297,34 @@ class SAI
         $commentType = str_replace(array_keys($match), array_values($match), $commentType);
 
         
+        if ($this->data['actions'][$actionIndex]['SAIAction'] == SMART_ACTION_TALK) {
+            $commentType = str_replace('_lineEntry_', $this->data['actions'][$actionIndex]['params'][0], $commentType);
+        }
+
         if ($this->_parent->dumpSpells) {
             // Prevent unnecessary processing
             if ($this->data['event_type'] == SMART_EVENT_SPELLHIT || $this->data['event_type'] == SMART_EVENT_SPELLHIT_TARGET) {
                 // For some bitch reason, some spellhit events have 0 as the spell hitter
                 if ($this->data['event_params'][1] != 0) {
-                    $record = Factory::createOrGetDBCWorker()->getRecordById($this->data['event_params'][1])->extract();
-                    $commentType = str_replace('_spellHitSpellId_', $record['SpellNameLang0'], $commentType); # Use your own locale here. I do not have english DBCs.
-                    unset($record); // Save memory
+                    $commentType = str_replace(
+                        '_spellHitSpellId_',
+                        Factory::createOrGetDBCWorker()->getRecordById($this->data['event_params'][1])->get('SpellNameLang0', DBC::STRING),
+                        $commentType); # Use your own locale here. I do not have english DBCs.
                 }
                 else
-                    $commentType = str_replace('_spellHitSpellId_', 'NOT FOUND', $commentType);
+                    $commentType = str_replace('_spellHitSpellId_', $this->data['event_params'][1] . '(Not found in DBCs)', $commentType);
             }
-
-            if ($this->data['actions'][$actionIndex]['SAIAction'] == SMART_ACTION_CAST) {
-                $record = Factory::createOrGetDBCWorker()->getRecordById($this->data['actions'][$actionIndex]['params'][0])->extract();
-                $commentType = str_replace('_castSpellId_', $record['SpellNameLang0'], $commentType);
-                unset($record);
+            elseif ($this->data['actions'][$actionIndex]['SAIAction'] == SMART_ACTION_CAST) {
+                $commentType = str_replace(
+                    '_castSpellId_',
+                    Factory::createOrGetDBCWorker()->getRecordById($this->data['actions'][$actionIndex]['params'][0])->get('SpellNameLang0', DBC::STRING),
+                    $commentType);
             }
-
-            if ($this->data['actions'][$actionIndex]['SAIAction'] == SMART_ACTION_REMOVEAURASFROMSPELL && $this->data['actions'][$actionIndex]['params'][0] != 0) {
-                $record = Factory::createOrGetDBCWorker()->getRecordById($this->data['actions'][$actionIndex]['params'][0])->extract();
-                $commentType = str_replace('_removeAuraSpell_', $record['SpellNameLang0'], $commentType);
-                unset($record);
+            elseif ($this->data['actions'][$actionIndex]['SAIAction'] == SMART_ACTION_REMOVEAURASFROMSPELL && $this->data['actions'][$actionIndex]['params'][0] != 0) {
+                $commentType = str_replace(
+                    '_removeAuraSpell_',
+                    Factory::createOrGetDBCWorker()->getRecordById($this->data['actions'][$actionIndex]['params'][0])->get('SpellNameLang0', DBC::STRING),
+                    $commentType);
             }
         }
         else
@@ -327,10 +332,10 @@ class SAI
             if ($this->data['actions'][$actionIndex]['SAIAction'] == SMART_ACTION_CAST)
                 $commentType = str_replace('_castSpellId_', $this->data['actions'][$actionIndex]['params'][0] . " (Not found in DBCs!)", $commentType);
 
-            if ($this->data['event_type'] == SMART_EVENT_SPELLHIT || $this->data['event_type'] == SMART_EVENT_SPELLHIT_TARGET)
+            elseif ($this->data['event_type'] == SMART_EVENT_SPELLHIT || $this->data['event_type'] == SMART_EVENT_SPELLHIT_TARGET)
                 $commentType = str_replace('_spellHitSpellId_', $this->data['event_params'][1] . " (Not found in DBCs!)", $commentType);
 
-            if ($this->data['actions'][$actionIndex]['SAIAction'] == SMART_ACTION_REMOVEAURASFROMSPELL && $this->data['actions'][$actionIndex]['params'][0] != 0)
+            elseif ($this->data['actions'][$actionIndex]['SAIAction'] == SMART_ACTION_REMOVEAURASFROMSPELL && $this->data['actions'][$actionIndex]['params'][0] != 0)
                 $commentType = str_replace('_removeAuraSpell_', $this->data['actions'][$actionIndex]['params'][0] . " (Not found in DBCs!)", $commentType);
         }
         // Some other parsing and fixing may be needed here

PHP/utils.php
@@ -235,7 +235,7 @@ class Utils
                         'SAIAction'   => SMART_ACTION_TALK,
                         'params'      => array($param1, $param2, $param3, 0, 0, 0),
                         'target'      => SMART_TARGET_NONE,
-                        'commentType' => "_npcName_ - _eventName_ - Say Line " . $param1
+                        'commentType' => "_npcName_ - _eventName_ - Say Line _lineEntry_"
                     );
                     break;
                 case ACTION_T_SET_FACTION:

commit 5484ca6a440deda414869ec726357b9acadd8ca6

Misc:
* Fix some mistakes in 0ab3b6a23bd29b604d4244e76302edd4590bb1f8
* Fix wrong elseif usage in 8ceba09f902c456b579ef6a2e509a4297dd33492, caused actions that required a DBC-lookup to be ignored if the event also required this.

PHP/eai.def.php
@@ -30,7 +30,7 @@ define('EVENT_T_FRIENDLY_MISSING_BUFF',   16); // SpellId, Radius, RepeatMin, Re
 define('EVENT_T_SUMMONED_UNIT',           17); // CreatureId, RepeatMin, RepeatMax
 define('EVENT_T_TARGET_MANA',             18); // ManaMax%, ManaMin%, RepeatMin, RepeatMax
 define('EVENT_T_QUEST_ACCEPT',            19); // QuestID
-define('EVENT_T_QUEST_COMPLETE',          20); //
+define('EVENT_T_QUEST_COMPLETE',          20); // 
 define('EVENT_T_REACHED_HOME',            21); // NONE
 define('EVENT_T_RECEIVE_EMOTE',           22); // EmoteId, Condition, CondValue1, CondValue2
 define('EVENT_T_BUFFED',                  23); // Param1 = SpellID, Param2 = Number of Time STacked, Param3/4 Repeat Min/Max

PHP/sqlbuilder.class.php
@@ -271,7 +271,7 @@ class SAI
                 $outputString .= $summonData->orientation . ',';
             }
             else
-                $outputString .= ',0,0,0,0,0,0,0,';
+                $outputString .= '0,0,0,0,0,0,0,';
 
             # Build the comment, and we're done.
             
@@ -301,29 +301,32 @@ class SAI
             $commentType = str_replace('_lineEntry_', $this->data['actions'][$actionIndex]['params'][0], $commentType);
         }
 
-        if ($this->_parent->dumpSpells) {
-            // Prevent unnecessary processing
+        // Any DBC-needed data is dumped here
+        if (($factory = Factory::createOrGetDBCWorker()) !== false) {
+            // Place event precessors here
             if ($this->data['event_type'] == SMART_EVENT_SPELLHIT || $this->data['event_type'] == SMART_EVENT_SPELLHIT_TARGET) {
                 // For some bitch reason, some spellhit events have 0 as the spell hitter
                 if ($this->data['event_params'][1] != 0) {
                     $commentType = str_replace(
                         '_spellHitSpellId_',
-                        Factory::createOrGetDBCWorker()->getRecordById($this->data['event_params'][1])->get('SpellNameLang0', DBC::STRING),
+                        $factory->getRecordById($this->data['event_params'][1])->get('SpellNameLang0', DBC::STRING),
                         $commentType); # Use your own locale here. I do not have english DBCs.
                 }
                 else
                     $commentType = str_replace(' _spellHitSpellId_', '', $commentType);
             }
-            elseif ($this->data['actions'][$actionIndex]['SAIAction'] == SMART_ACTION_CAST) {
+            
+            // Place action processors here
+            if ($this->data['actions'][$actionIndex]['SAIAction'] == SMART_ACTION_CAST) {
                 $commentType = str_replace(
                     '_castSpellId_',
-                    Factory::createOrGetDBCWorker()->getRecordById($this->data['actions'][$actionIndex]['params'][0])->get('SpellNameLang0', DBC::STRING),
+                    $factory->getRecordById($this->data['actions'][$actionIndex]['params'][0])->get('SpellNameLang0', DBC::STRING),
                     $commentType);
             }
             elseif ($this->data['actions'][$actionIndex]['SAIAction'] == SMART_ACTION_REMOVEAURASFROMSPELL && $this->data['actions'][$actionIndex]['params'][0] != 0) {
                 $commentType = str_replace(
                     '_removeAuraSpell_',
-                    Factory::createOrGetDBCWorker()->getRecordById($this->data['actions'][$actionIndex]['params'][0])->get('SpellNameLang0', DBC::STRING),
+                    $factory->getRecordById($this->data['actions'][$actionIndex]['params'][0])->get('SpellNameLang0', DBC::STRING),
                     $commentType);
             }
         }

PHP/utils.php
@@ -232,14 +232,14 @@ class Utils
                     $result[$i] = array(
                         'extraData'   => $pdo->query("SELECT * FROM `creature_ai_texts` WHERE `entry` IN (" . $param1 . "," . $param2 . "," . $param3 . ")")->fetchAll(PDO::FETCH_OBJ),
                         'SAIAction'   => SMART_ACTION_TALK,
-                        'params'      => array($param1, $param2, $param3, 0, 0, 0)
+                        'params'      => array($param1, $param2, $param3, 0, 0, 0),
                         'commentType' => "_npcName_ - _eventName_ - Say Line _lineEntry_"
                     );
                     break;
                 case ACTION_T_SET_FACTION:
                     $result[$i] = array(
                         'SAIAction'   => SMART_ACTION_SET_FACTION,
-                        'params'      => array($param1, $param2, $param3, 0, 0, 0)
+                        'params'      => array($param1, $param2, $param3, 0, 0, 0),
                         'commentType' => "_npcName_ - _eventName_ - Set Faction " . $param1
                     );
                     break;
@@ -297,7 +297,7 @@ class Utils
                 case ACTION_T_QUEST_EVENT:
                     $result[$i] = array(
                         'SAIAction'  => SMART_ACTION_CALL_AREAEXPLOREDOREVENTHAPPENS,
-                        'params'     => array($param1, 0, 0, 0, 0, 0)
+                        'params'     => array($param1, 0, 0, 0, 0, 0),
                         'commentType' => "_npcName_ - _eventName_ - Quest Credit"
                     );
                     if ($eaiAction == ACTION_T_QUEST_EVENT)
@@ -307,7 +307,7 @@ class Utils
                 case ACTION_T_CAST_EVENT:
                     $result[$i] = array(
                         'SAIAction'  => SMART_ACTION_SEND_CASTCREATUREORGO,
-                        'params'     => array($param1, $param2, 0, 0, 0, 0)
+                        'params'     => array($param1, $param2, 0, 0, 0, 0),
                         'commentType' => "_npcName_ - _eventName_ - Quest Credit"
                     );
                     if ($eaiAction == ACTION_T_CAST_EVENT)
@@ -342,21 +342,21 @@ class Utils
                 case ACTION_T_AUTO_ATTACK:
                     $result[$i] = array(
                         'SAIAction'  => SMART_ACTION_AUTO_ATTACK,
-                        'params'     => array($param1, 0, 0, 0, 0, 0)
+                        'params'     => array($param1, 0, 0, 0, 0, 0),
                         'commentType' => "_npcName_ - _eventName_ - Start Auto Attack"
                     );
                     break;
                 case ACTION_T_COMBAT_MOVEMENT:
                     $result[$i] = array(
                         'SAIAction'  => SMART_ACTION_ALLOW_COMBAT_MOVEMENT,
-                        'params'     => array($param1, 0, 0, 0, 0, 0)
+                        'params'     => array($param1, 0, 0, 0, 0, 0),
                         'commentType' => "_npcName_ - _eventName_ - Allow Combat Movement"
                     );
                     break;
                 case ACTION_T_SET_PHASE:
                     $result[$i] = array(
                         'SAIAction'  => SMART_ACTION_SET_EVENT_PHASE,
-                        'params'     => array($param1, 0, 0, 0, 0, 0)
+                        'params'     => array($param1, 0, 0, 0, 0, 0),
                         'commentType' => "_npcName_ - _eventName_ - Set Phase " . $param1
                     );
                     break;
@@ -365,14 +365,14 @@ class Utils
                     return sLog::outString('Tried to cast ACTION_T_INC_PHASE to SMART_ACTION_INC_EVENT_PHASE, but parameters are not totally handled! Aborting');
                     $result[$i] = array(
                         'SAIAction'  => SMART_ACTION_INC_EVENT_PHASE,
-                        'params'     => array(__FIXME__, __FIXME__, __FIXME__, __FIXME__, __FIXME__, __FIXME__)
+                        'params'     => array(__FIXME__, __FIXME__, __FIXME__, __FIXME__, __FIXME__, __FIXME__),
                         'commentType' => "_npcName_ - _eventName_ - Increment Phase"
                     );
                     break;
                 case ACTION_T_EVADE:
                     $result[$i] = array(
                         'SAIAction'  => SMART_ACTION_EVADE,
-                        'params'     => array(0, 0, 0, 0, 0, 0)
+                        'params'     => array(0, 0, 0, 0, 0, 0),
                         'commentType' => "_npcName_ - _eventName_ - Evade"
                     );
                     break;
@@ -408,7 +408,7 @@ class Utils
                 case ACTION_T_RANDOM_PHASE:
                     $result[$i] = array(
                         'SAIAction'  => SMART_ACTION_RANDOM_PHASE,
-                        'params'     => array($param1, $param2, $param3, 0, 0, 0)
+                        'params'     => array($param1, $param2, $param3, 0, 0, 0),
                         'commentType' => "_npcName_ - _eventName_ - Set Random Phase"
                     );
                     break;
@@ -416,7 +416,7 @@ class Utils
                     //! TODO: Check if EAI is inclusive or exclusive (like SAI)
                     $result[$i] = array(
                         'SAIAction'  => SMART_ACTION_RANDOM_PHASE_RANGE,
-                        'params'     => array($param1, $param2)
+                        'params'     => array($param1, $param2, 0, 0, 0, 0),
                         'commentType' => "_npcName_ - _eventName_ - Random Phase Range (${param1} - ${param2})" // Because i'm tired of concatenating
                     );
                     break;
@@ -433,7 +433,7 @@ class Utils
                     $result[$i] = array(
                         'extraData'     => $pdo->query("SELECT * FROM `creature_ai_summons` WHERE `id`=" . $param3)->fetch(PDO::FETCH_OBJ),
                         'SAIAction'     => SMART_ACTION_SUMMON_CREATURE,
-                        'params'        => array($param1, 0, 0, 0, 0, 0)
+                        'params'        => array($param1, 0, 0, 0, 0, 0),
                         'commentType'   => "_npcName_ - _eventName_ - Summon Creature " . $pdo->query("SELECT `name` FROM `creature_template` WHERE `entry`=${param1}")->fetch(PDO::FETCH_OBJ)->name,
                         'isSpecialHandler' => true,
                     );
@@ -449,7 +449,7 @@ class Utils
                 case ACTION_T_SET_INST_DATA:
                     $result[$i] = array(
                         'SAIAction'  => SMART_ACTION_SET_INST_DATA,
-                        'params'     => array($param1, $param2)
+                        'params'     => array($param1, $param2, 0, 0, 0, 0),
                         'commentType' => "_npcName_ - _eventName_ - Set Instance Data ${param1} to ${param2}"
                     );
                     break;
@@ -464,35 +464,35 @@ class Utils
                 case ACTION_T_UPDATE_TEMPLATE:
                     $result[$i] = array(
                         'SAIAction'  => SMART_ACTION_UPDATE_TEMPLATE,
-                        'params'     => array($param1, $param2, 0, 0, 0, 0)
+                        'params'     => array($param1, $param2, 0, 0, 0, 0),
                         'commentType' => "_npcName_ - _eventName_ - Update Template"
                     );
                     break;
                 case ACTION_T_DIE:
                     $result[$i] = array(
                         'SAIAction'  => SMART_ACTION_DIE,
-                        'params'     => array(0, 0, 0, 0, 0, 0)
+                        'params'     => array(0, 0, 0, 0, 0, 0),
                         'commentType' => "_npcName_ - _eventName_ - Die"
                     );
                     break;
                 case ACTION_T_ZONE_COMBAT_PULSE:
                     $result[$i] = array(
                         'SAIAction'  => SMART_ACTION_SET_IN_COMBAT_WITH_ZONE,
-                        'params'     => array(0, 0, 0, 0, 0, 0)
+                        'params'     => array(0, 0, 0, 0, 0, 0),
                         'commentType' => "_npcName_ - _eventName_ - Set In Combat With Zone"
                     );
                     break;
                 case ACTION_T_CALL_FOR_HELP:
                     $result[$i] = array(
                         'SAIAction'  => SMART_ACTION_CALL_FOR_HELP,
-                        'params'     => array($param1, 0, 0, 0, 0, 0)
+                        'params'     => array($param1, 0, 0, 0, 0, 0),
                         'commentType' => "_npcName_ - _eventName_ - Call For Help"
                     );
                     break;
                 case ACTION_T_SET_SHEATH:
                     $result[$i] = array(
                         'SAIAction'  => SMART_ACTION_SET_SHEATH,
-                        'params'     => array($param1, 0, 0, 0, 0, 0)
+                        'params'     => array($param1, 0, 0, 0, 0, 0),
                         'commentType' => "_npcName_ - _eventName_ - "
                     );
 
@@ -512,21 +512,21 @@ class Utils
                 case ACTION_T_FORCE_DESPAWN:
                     $result[$i] = array(
                         'SAIAction'  => SMART_ACTION_FORCE_DESPAWN,
-                        'params'     => array($param1, 0, 0, 0, 0, 0)
+                        'params'     => array($param1, 0, 0, 0, 0, 0),
                         'commentType' => "_npcName_ - _eventName_ - Forced Despawn"
                     );
                     break;
                 case ACTION_T_SET_INVINCIBILITY_HP_LEVEL:
                     $result[$i] = array(
                         'SAIAction'  => SMART_ACTION_SET_INVINCIBILITY_HP_LEVEL,
-                        'params'     => array($param1, 0, 0, 0, 0, 0)
+                        'params'     => array($param1, 0, 0, 0, 0, 0),
                         'commentType' => "_npcName_ - _eventName_ - Set Invincibility Health Pct To " . $param1
                     );
                     break;
                 case ACTION_T_MOUNT_TO_ENTRY_OR_MODEL:
                     $result[$i] = array(
                         'SAIAction'  => SMART_ACTION_MOUNT_TO_ENTRY_OR_MODEL,
-                        'params'     => array($param1, $param2, 0, 0, 0, 0)
+                        'params'     => array($param1, $param2, 0, 0, 0, 0),
                         'commentType' => "_npcName_ - _eventName_ - Mount Up"
                     );
 
@@ -536,7 +536,7 @@ class Utils
                 case ACTION_T_SET_PHASE_MASK:
                     $result[$i] = array(
                         'SAIAction'  => SMART_ACTION_SET_INGAME_PHASE_MASK,
-                        'params'     => array($param1, 0, 0, 0, 0, 0)
+                        'params'     => array($param1, 0, 0, 0, 0, 0),
                         'commentType' => "_npcName_ - _eventName_ - Set Phase"
                     );
                     break;
@@ -551,14 +551,14 @@ class Utils
                 case ACTION_T_MOVE_RANDOM_POINT:
                     $result[$i] = array(
                         'SAIAction'  => SMART_ACTION_RANDOM_MOVE,
-                        'params'     => array($param1, 0, 0, 0, 0, 0)
+                        'params'     => array($param1, 0, 0, 0, 0, 0),
                         'commentType' => "_npcName_ - _eventName_ - Move Random"
                     );
                     break;
                 case ACTION_T_SET_VISIBILITY:
                     $result[$i] = array(
                         'SAIAction'  => SMART_ACTION_SET_VISIBILITY,
-                        'params'     => array($param1, 0, 0, 0, 0, 0)
+                        'params'     => array($param1, 0, 0, 0, 0, 0),
                         'commentType' => "_npcName_ - _eventName_ - Set Visiblity"
                     );
                     break;
@@ -566,35 +566,35 @@ class Utils
                     //! SAI has no parameter and cannot set a NPC as inactive!
                     $result[$i] = array(
                         'SAIAction'  => SMART_ACTION_SET_ACTIVE,
-                        'params'     => array(0, 0, 0, 0, 0, 0)
+                        'params'     => array(0, 0, 0, 0, 0, 0),
                         'commentType' => "_npcName_ - _eventName_ - Set Active"
                     );
                     break;
                 case ACTION_T_SET_AGGRESSIVE:
                     $result[$i] = array(
                         'SAIAction'  => SMART_ACTION_SET_REACT_STATE,
-                        'params'     => array($param1, 0, 0, 0, 0, 0)
+                        'params'     => array($param1, 0, 0, 0, 0, 0),
                         'commentType' => "_npcName_ - _eventName_ - Set React Aggressive"
                     );
                     break;
                 case ACTION_T_ATTACK_START_PULSE:
                     $result[$i] = array(
                         'SAIAction'  => SMART_ACTION_ATTACK_START,
-                        'params'     => array(0, 0, 0, 0, 0, 0)
+                        'params'     => array(0, 0, 0, 0, 0, 0),
                         'commentType' => "_npcName_ - _eventName_ - Attack Start"
                     );
                     break;
                 case ACTION_T_SUMMON_GO:
                     $result[$i] = array(
                         'SAIAction'  => SMART_ACTION_SUMMON_GO,
-                        'params'     => array($param1, $param2, 0, 0, 0, 0)
+                        'params'     => array($param1, $param2, 0, 0, 0, 0),
                         'commentType' => "_npcName_ - _eventName_ - Summon Gameobject " . $pdo->query("SELECT name FROM gameobject_template WHERE entry = ${param1}")->fetch(PDO::FETCH_OBJ)->name
                     );
                     break;
                 case ACTION_T_NONE:
                     $result[$i] = array(
                         'SAIAction'  => SMART_ACTION_NONE,
-                        'params'     => array(0, 0, 0, 0, 0, 0)
+                        'params'     => array(0, 0, 0, 0, 0, 0),
                         'commentType' => "_npcName_ - _eventName_ - UNUSED"
                     );
                     break;
@@ -606,7 +606,7 @@ class Utils
                 default:
                     $result[$i] = array(
                         'SAIAction'  => __FIXME__,
-                        'params'     => array(__FIXME__, __FIXME__, __FIXME__, __FIXME__, __FIXME__, __FIXME__)
+                        'params'     => array(__FIXME__, __FIXME__, __FIXME__, __FIXME__, __FIXME__, __FIXME__),
                         'commentType' => "_npcName_ - _eventName_ - Y me not working ??? :("
                     );
                     break;